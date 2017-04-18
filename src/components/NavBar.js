73cf5c836fa033ab61016107b571f0d5a4e898a2
David Armbrust <david.armbrust@gmail.com>
2017-04-18 14:56:49 -0500

    Update route to ignore jsessionId in url

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 import NavButton from '../components/NavButton.js'
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { fetchPlaylists } from '../actions/playlists_action.js';
-import { fetchUserInfo } from '../actions/userInfo_actions.js';
 
 class NavBar extends Component {
   render() {
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Route, IndexRoute, Redirect } from 'react-router';
 
 import App from './components/App.js';
 import userInfo from './containers/userInfo.js';
@@ -9,6 +9,7 @@ import index from './components/index.js'
 export default (
   < Route path="/" component={App} >
     <IndexRoute component={index} />
+    <Redirect from='/;*' to='/' />
     <Route path="main" component={index} />
     <Route path="user" component={userInfo} />
     <Route path="playlists" component={playlistList} />
